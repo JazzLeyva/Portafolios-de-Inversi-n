49bf1d11b3d0d3616dce24204c6cc4e30fe89f46
JehuSilva <ijehu.silva@gmail.com>
2021-10-11 07:40:59 -0500

    Datasets generados

---
+++ R/crear_datos_de_simulacion.R
@@ -0,0 +1,53 @@
+# load the functions, libraries etc
+source("R/functions.R")
+setwd("c:/Users/Jehu/Documents/R/Portafolios-de-Inversi-n/")
+
+# SELECCIONA TU EXPERIMENTO, OPCIONES (1,2 3)
+EXPERIMENTO = 2
+
+
+
+# GENERACION DE DATOS ALEATORIOS BASADO EN LA MEDIA Y VARIANZA 
+# QUE GENERO CADA ACCION
+
+if (EXPERIMENTO == 1) { # FALSE, VOO Y MCN
+    x_m = 0.0088
+    x_sd = 0.1423
+    y_m = 0.0107 
+    y_sd = 0.0387
+    z_m = -0.0019
+    z_sd = 0.0488
+    
+} else {
+    if(EXPERIMENTO == 2) { # NEE, NESN.S Y NVDA
+        x_m = 0.0122
+        x_sd = 0.0495
+        y_m = 0.0074 
+        y_sd = 0.0366
+        x_m = 0.0340 
+        z_sd = 0.1322
+    } else {# NIO, PALAF Y QQQ
+        x_m = 0.1058
+        x_sd = 0.3497
+        y_m = 0.0743
+        y_sd = 0.6627
+        z_m = 0.0127
+        z_sd = 0.0508
+    }
+} 
+
+set.seed(13254)
+df <- data.table(x = rnorm(10000, mean = x_m, sd = x_sd))
+
+y_mean <- y_m
+y_sd <- y_sd
+
+z_mean <- z_m
+z_sd <- z_sd
+
+df[, y := rmultvar(x, r = 0, y_mean, y_sd)]
+df[, z := rmultvar(x, r = 0, z_mean, z_sd)]
+
+write.csv(df, file = paste(c("data/Experimentos/Experimento",EXPERIMENTO,"csv"),collapse=""), row.names = F)
+
+
---
+++ R/plot_market.R
@@ -0,0 +1,98 @@
+library(data.table)
+library(scales)
+library(ggplot2)
+
+
+# Directorio de trabajo
+setwd("c:/Users/Jehu/Documents/R/Portafolios-de-Inversi-n/")
+
+# SELECCIONA TU EXPERIMENTO, OPCIONES (1,2 3)
+EXPERIMENTO = 3
+
+
+# Descargando datasets de acciones
+
+link1 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/F.csv"
+link2 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/VOO.csv"
+link3 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/MCN.csv"
+link4 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/NEE.csv"
+link5 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/NESN.SW.csv"
+link6 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/NVDA.csv"
+link7 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/NIO.csv"
+link8 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/PALAF.csv"
+link9 <- "https://raw.githubusercontent.com/JazzLeyva/Portafolios-de-Inversi-n/master/Data/Mercados/QQQ.csv"
+
+dt1 <-data.table(read.csv(link1, header=TRUE))
+dt2 <-data.table(read.csv(link2, header=TRUE))
+dt3 <-data.table(read.csv(link3, header=TRUE))
+dt4 <-data.table(read.csv(link4, header=TRUE))
+dt5 <-data.table(read.csv(link5, header=TRUE))
+dt6 <-data.table(read.csv(link6, header=TRUE))
+dt7 <-data.table(read.csv(link7, header=TRUE))
+dt8 <-data.table(read.csv(link8, header=TRUE))
+dt9 <-data.table(read.csv(link9, header=TRUE))
+
+
+if (EXPERIMENTO == 1) { # FALSE, VOO Y MCN
+  dt1 <-data.table(read.csv(link1, header=TRUE))
+  dt2 <-data.table(read.csv(link2, header=TRUE))
+  dt3 <-data.table(read.csv(link3, header=TRUE))
+    
+} else {
+    if(EXPERIMENTO == 2) { # NEE, NESN.S Y NVDA
+        dt1 <-data.table(read.csv(link4, header=TRUE))
+        dt2 <-data.table(read.csv(link5, header=TRUE))
+        dt3 <-data.table(read.csv(link6, header=TRUE))
+    } else {# NIO, PALAF Y QQQ
+        dt1 <-data.table(read.csv(link7, header=TRUE))
+        dt2 <-data.table(read.csv(link8, header=TRUE))
+        dt3 <-data.table(read.csv(link9, header=TRUE))
+    }
+} 
+
+
+
+dt = rbind(dt1, dt2, dt3)
+
+# Convertimos la columna data en un elemento de tiempo
+dt[, date := as.Date(date)]
+
+# Agregamos una columna con valores indexados
+dt[, idx_price := price/price[1], by = ticker]
+
+
+# plot the indexed values
+plot_three <- ggplot(dt, aes(x = date, y = idx_price, color = ticker)) +
+  geom_line() +
+  # Miscellaneous Formatting
+  theme_bw() + ggtitle("Precio desarrollado") +
+  xlab("Fecha") + ylab("Precio\n(Indexed 2000 = 1)") +
+  scale_color_discrete(name = "Compania")
+ggsave(filename =  paste(c("resultados/Experimento",EXPERIMENTO,"/grafica_de_acciones.png"),collapse=""), plot_three, scale = 1.5, dpi=600)
+
+
+# Calculando los retornos aritmeticos
+dt[, ret := price / shift(price, 1) - 1, by = ticker]
+ 
+# Resumen de tabla
+# take only non-na values
+tab <- dt[!is.na(ret), .(ticker, ret)]
+ 
+# calculate the expected returns (historical mean of returns) and volatility (standard deviation of returns)
+# Calculando los valores esperados (media de los datos historicos)
+# y (Desviacion estandar)
+tab <- tab[, .(er = round(mean(ret), 4), 
+               sd = round(sd(ret), 4)), 
+           by = "ticker"]
+tab
+
+# Graficando resultados
+plot_three <- ggplot(tab, aes(x = sd, y = er, color = ticker)) +
+  geom_point(size = 5) +
+  # Miscellaneous Formatting
+  theme_bw() + ggtitle("Compensacion riesgo - rendimiento") +
+  xlab("Volatilidad") + ylab("Respuesta esperada") +
+  scale_y_continuous(label = percent, limits = c(0.0, 0.2)) +
+  scale_x_continuous(label = percent, limits = c(0.0, 0.7))
+
+ggsave(filename =  paste(c("resultados/Experimento",EXPERIMENTO,"/grafica_de_correlaciones.png"),collapse=""), plot_three, scale = 1.5, dpi=600)
